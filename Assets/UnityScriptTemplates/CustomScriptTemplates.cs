07c32c5154a0ddc23fde3382e373af50bdc5d666
Baste Nesse Buanes <baste@rain-games.com>
2018-07-07 22:21:07 +0200

    First pass on window to edit script templates.

--- Assets/UnityScriptTemplates/CustomScriptTemplates.cs
+++ Assets/UnityScriptTemplates/CustomScriptTemplates.cs
@@ -3,77 +3,76 @@ using System.Reflection;
 using UnityEditor;
 using UnityEngine;
 
-/// <summary>
-/// This file adds a menu item just below "Create->C# Script" for creating some different script templates.
-/// You can add your own templates by copy-pasting the code in this file and editing the paths.a
-/// </summary>
-public static class CustomScriptTemplates {
+namespace UnityScriptTemplates {
+
     /// <summary>
-    /// Path to the template file for a ScriptableObject.
-    /// Using .cs.txt extension to match Unity's built-in templates, but this is _not_ neccessary.
+    /// This file adds a menu item just below "Create->C# Script" for creating some different script templates.
+    /// You can add your own templates by copy-pasting the code in this file and editing the paths.a
     /// </summary>
-    private static string TemplatePathScriptableObject {
-        get {
-            return Application.dataPath + "/EditorDefaultResources/ScriptableObjectTemplate.cs.txt";
+    public static class CustomScriptTemplates {
+        /// <summary>
+        /// Path to the template file for a ScriptableObject.
+        /// Using .cs.txt extension to match Unity's built-in templates, but this is _not_ neccessary.
+        /// </summary>
+        private static string TemplatePathScriptableObject {
+            get { return Application.dataPath + "/EditorDefaultResources/ScriptTemplates/ScriptableObjectTemplate.cs.txt"; }
         }
-    }
 
-    /// <summary>
-    /// Path to the template file for a Plain C# class.
-    /// </summary>
-    private static string TemplatePathPlainClass {
-        get {
-            return Application.dataPath + "/EditorDefaultResources/PlainClassTemplate.cs.txt";
+        /// <summary>
+        /// Path to the template file for a Plain C# class.
+        /// </summary>
+        private static string TemplatePathPlainClass {
+            get { return Application.dataPath + "/EditorDefaultResources/ScriptTemplates/PlainClassTemplate.cs.txt"; }
         }
-    }
 
-    /// <summary>
-    /// ProjectWindowUtil.CreateScriptAsset is the method that makes the magic happen.
-    /// It has two parameters:
-    /// - templatePath, the absolute path to the template file.
-    /// - destName, the suggested file name for the new asset.
-    ///
-    /// It seems like this method is usually called from c++; it's a private method, and nothing in ProjectWindowUtil calls it, but if you add a breakpoint
-    /// in it when hitting Create-C# script, the breakpoint is hit, with no stack trace.
-    /// </summary>
-    private static MethodInfo CreateScriptAsset {
-        get {
-            var projectWindowUtilType = typeof(ProjectWindowUtil);
-            return  projectWindowUtilType.GetMethod("CreateScriptAsset", BindingFlags.NonPublic | BindingFlags.Static);
+        /// <summary>
+        /// ProjectWindowUtil.CreateScriptAsset is the method that makes the magic happen.
+        /// It has two parameters:
+        /// - templatePath, the absolute path to the template file.
+        /// - destName, the suggested file name for the new asset.
+        ///
+        /// It seems like this method is usually called from c++; it's a private method, and nothing in ProjectWindowUtil calls it, but if you add a breakpoint
+        /// in it when hitting Create-C# script, the breakpoint is hit, with no stack trace.
+        /// </summary>
+        private static MethodInfo CreateScriptAsset {
+            get {
+                var projectWindowUtilType = typeof(ProjectWindowUtil);
+                return projectWindowUtilType.GetMethod("CreateScriptAsset", BindingFlags.NonPublic | BindingFlags.Static);
+            }
         }
-    }
 
-    /// <summary>
-    /// Adds a menu item for creating a new ScriptableObject file.
-    /// </summary>
-    [MenuItem("Assets/Create/Script Template/ScriptableObject", priority = 81)] // Create/C# Script has priority 80, so this puts it just below that.
-    public static void CreateScriptableObject() {
-        CreateScriptAsset.Invoke(null, new object[] { TemplatePathScriptableObject, "NewScriptableObject.cs" });
-    }
+        /// <summary>
+        /// Adds a menu item for creating a new ScriptableObject file.
+        /// </summary>
+        [MenuItem("Assets/Create/Script Template/ScriptableObject", priority = 81)] // Create/C# Script has priority 80, so this puts it just below that.
+        public static void CreateScriptableObject() {
+            CreateScriptAsset.Invoke(null, new object[] { TemplatePathScriptableObject, "NewScriptableObject.cs" });
+        }
 
-    /// <summary>
-    /// Validates that the ScriptableObject template exists.
-    /// It's pretty important to check that the template path exist. If you call this method with an empty template Unity creates an invisible asset you cannot
-    /// interact with, which will stick around until you restart Unity. 
-    /// </summary>
-    [MenuItem("Assets/Create/Script Template/ScriptableObject", true, priority = 81)]
-    public static bool CreateScriptableObjectValidate() {
-        return File.Exists(TemplatePathScriptableObject) && CreateScriptAsset != null;
-    }
+        /// <summary>
+        /// Validates that the ScriptableObject template exists.
+        /// It's pretty important to check that the template path exist. If you call this method with an empty template Unity creates an invisible asset you cannot
+        /// interact with, which will stick around until you restart Unity. 
+        /// </summary>
+        [MenuItem("Assets/Create/Script Template/ScriptableObject", true, priority = 81)]
+        public static bool CreateScriptableObjectValidate() {
+            return File.Exists(TemplatePathScriptableObject) && CreateScriptAsset != null;
+        }
 
-    /// <summary>
-    /// Adds a menu item for creating a new plain class file.
-    /// </summary>
-    [MenuItem("Assets/Create/Script Template/Class", priority = 81)]
-    public static void CreatePlainClass() {
-        CreateScriptAsset.Invoke(null, new object[] { TemplatePathPlainClass, "NewClass.cs" });
-    }
+        /// <summary>
+        /// Adds a menu item for creating a new plain class file.
+        /// </summary>
+        [MenuItem("Assets/Create/Script Template/Class", priority = 81)]
+        public static void CreatePlainClass() {
+            CreateScriptAsset.Invoke(null, new object[] { TemplatePathPlainClass, "NewClass.cs" });
+        }
 
-    /// <summary>
-    /// Validates that the plain class template exists. 
-    /// </summary>
-    [MenuItem("Assets/Create/Script Template/Class", true, priority = 81)]
-    public static bool CreatePlainClassValidate() {
-        return File.Exists(TemplatePathPlainClass) && CreateScriptAsset != null;
+        /// <summary>
+        /// Validates that the plain class template exists. 
+        /// </summary>
+        [MenuItem("Assets/Create/Script Template/Class", true, priority = 81)]
+        public static bool CreatePlainClassValidate() {
+            return File.Exists(TemplatePathPlainClass) && CreateScriptAsset != null;
+        }
     }
 }
---
+++ Assets/UnityScriptTemplates/EditScriptTemplates.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.Principal;
+using UnityEditor;
+using UnityEngine;
+
+namespace UnityScriptTemplates {
+    public class EditScriptTemplates : EditorWindow {
+        [MenuItem("Window/Edit Script Templates")]
+        public static void OpenWindow() {
+            GetWindow<EditScriptTemplates>();
+        }
+
+        public List<ScriptTemplateFile> customScriptTemplates;
+        public List<ScriptTemplateFile> builtInScriptTemplates;
+
+        private ScriptTemplateFile selectedTemplate;
+        private bool hasWriteAccessToBuiltins;
+        private string builtInPath;
+        private string customPath;
+        private Vector2 scroll;
+
+        private void OnEnable() {
+            builtInPath = EditorApplication.applicationContentsPath + "/Resources/ScriptTemplates";
+            customPath = Application.dataPath + "/EditorDefaultResources/ScriptTemplates";
+            customScriptTemplates = FindScriptTemplates(customPath, false);
+            builtInScriptTemplates = FindScriptTemplates(builtInPath, true);
+
+            if (builtInScriptTemplates.Count > 0) {
+                try {
+                    builtInScriptTemplates[0].SaveToFile();
+                    hasWriteAccessToBuiltins = true;
+                }
+                catch(UnauthorizedAccessException) {
+                    hasWriteAccessToBuiltins = false;
+                }
+            }
+        }
+
+        private void OnGUI() {
+            scroll = EditorGUILayout.BeginScrollView(scroll);
+            EditorGUILayout.LabelField("Custom script templates (" + customPath + ")");
+            foreach (var scriptTemplate in customScriptTemplates) {
+                if (GUILayout.Button(scriptTemplate.name)) {
+                    selectedTemplate = scriptTemplate;
+                }
+            }
+
+            EditorGUILayout.LabelField("Built in script templates (" + builtInPath + ")");
+            foreach (var scriptTemplate in builtInScriptTemplates) {
+                if (GUILayout.Button(scriptTemplate.name)) {
+                    selectedTemplate = scriptTemplate;
+                }
+            }
+
+            if (selectedTemplate != null) {
+                var disabled = !hasWriteAccessToBuiltins && selectedTemplate.isBuiltIn;
+                if (disabled) {
+                    EditorGUILayout.LabelField("Cannot edit built in templates! You're probably not running Unity as administrator/root");
+                    EditorGUILayout.LabelField("Either relaunch as administrator/root, or edit the files directly in their folder");
+                }
+
+                EditorGUI.BeginDisabledGroup(disabled);
+                selectedTemplate.text = EditorGUILayout.TextArea(selectedTemplate.text);
+
+                EditorGUILayout.BeginHorizontal();
+
+                if (GUILayout.Button("Save to file", GUILayout.Width(120f))) {
+                    selectedTemplate.SaveToFile();
+                }
+
+                if (GUILayout.Button("Discard changes", GUILayout.Width(120f))) {
+                    selectedTemplate.DiscardChanges();
+                    GUI.FocusControl(null); //The text area doesn't update while not selected
+                    Repaint();
+                }
+
+                GUILayout.FlexibleSpace();
+
+                EditorGUILayout.LabelField("Indent style:", GUILayout.Width(100f));
+                if (GUILayout.Button("Spaces")) {
+                    selectedTemplate.text = selectedTemplate.text.Replace("\t", "    ");
+                }
+
+                if (GUILayout.Button("Tabs")) {
+                    selectedTemplate.text = selectedTemplate.text.Replace("    ", "\t");
+                }
+
+                EditorGUI.EndDisabledGroup();
+
+                EditorGUILayout.EndHorizontal();
+            }
+            EditorGUILayout.EndScrollView();
+        }
+
+        private static List<ScriptTemplateFile> FindScriptTemplates(string templateFolder, bool isInternal) {
+            return Directory.GetFiles(templateFolder)
+                            .Where(file => file.EndsWith(".cs.txt"))
+                            .Select(file => new ScriptTemplateFile(file, isInternal))
+                            .ToList();
+        }
+    }
+
+    public class ScriptTemplateFile {
+        private readonly string path;
+        public readonly string name;
+        public readonly bool isBuiltIn;
+
+        private string fileText;
+        public string text;
+
+        public ScriptTemplateFile(string path, bool isBuiltIn) {
+            this.path = path;
+            this.isBuiltIn = isBuiltIn;
+            name = Path.GetFileNameWithoutExtension(path);
+            fileText = File.ReadAllText(path);
+            text = fileText;
+        }
+
+        public void SaveToFile() {
+            File.WriteAllText(path, text);
+            fileText = text;
+            if (!isBuiltIn) {
+                var assetDatabasePath = "Assets" + path.Substring(Application.dataPath.Length);
+                AssetDatabase.ImportAsset(assetDatabasePath);
+            }
+        }
+
+        public void DiscardChanges() {
+            text = fileText;
+        }
+    }
+}
