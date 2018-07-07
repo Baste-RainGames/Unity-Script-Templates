24ea9caceeb76b4b7d201e0888cb6ca0c0c46f12
Baste Nesse Buanes <baste@rain-games.com>
2018-07-07 23:13:21 +0200

    Finished the edit templates window. Updated readme.

--- Assets/UnityScriptTemplates/EditScriptTemplates.cs
+++ Assets/UnityScriptTemplates/EditScriptTemplates.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Security.Principal;
 using UnityEditor;
 using UnityEngine;
 
@@ -16,8 +15,14 @@ namespace UnityScriptTemplates {
         public List<ScriptTemplateFile> customScriptTemplates;
         public List<ScriptTemplateFile> builtInScriptTemplates;
 
+        //serialized by Unity so selection can survive script reload
+        public bool selectedIsBuiltIn;
+        public int selectedIndex = -1;
+
+        private GenericMenu selectTemplateMenu;
         private ScriptTemplateFile selectedTemplate;
         private bool hasWriteAccessToBuiltins;
+
         private string builtInPath;
         private string customPath;
         private Vector2 scroll;
@@ -25,74 +30,113 @@ namespace UnityScriptTemplates {
         private void OnEnable() {
             builtInPath = EditorApplication.applicationContentsPath + "/Resources/ScriptTemplates";
             customPath = Application.dataPath + "/EditorDefaultResources/ScriptTemplates";
-            customScriptTemplates = FindScriptTemplates(customPath, false);
             builtInScriptTemplates = FindScriptTemplates(builtInPath, true);
+            customScriptTemplates = FindScriptTemplates(customPath, false);
+
+            var listToSelectIn = selectedIsBuiltIn ? builtInScriptTemplates : customScriptTemplates;
+            if (selectedIndex > 0 && selectedIndex < listToSelectIn.Count)
+                selectedTemplate = listToSelectIn[selectedIndex];
 
             if (builtInScriptTemplates.Count > 0) {
                 try {
                     builtInScriptTemplates[0].SaveToFile();
                     hasWriteAccessToBuiltins = true;
                 }
-                catch(UnauthorizedAccessException) {
+                catch (UnauthorizedAccessException) {
                     hasWriteAccessToBuiltins = false;
                 }
             }
+
+            selectTemplateMenu = new GenericMenu();
+            foreach (var template in customScriptTemplates) {
+                selectTemplateMenu.AddItem(new GUIContent("Custom/" + template.name), false, TemplateSelected, template);
+            }
+
+            foreach (var template in builtInScriptTemplates) {
+                selectTemplateMenu.AddItem(new GUIContent("Built in/" + template.name), false, TemplateSelected, template);
+            }
+        }
+
+        private void TemplateSelected(object template) {
+            selectedTemplate = (ScriptTemplateFile) template;
         }
 
         private void OnGUI() {
             scroll = EditorGUILayout.BeginScrollView(scroll);
-            EditorGUILayout.LabelField("Custom script templates (" + customPath + ")");
-            foreach (var scriptTemplate in customScriptTemplates) {
-                if (GUILayout.Button(scriptTemplate.name)) {
-                    selectedTemplate = scriptTemplate;
-                }
+            DrawTemplateSelection();
+
+            if (selectedTemplate != null) {
+                EditorGUILayout.Space();
+                DrawSelectedTemplate();
             }
 
-            EditorGUILayout.LabelField("Built in script templates (" + builtInPath + ")");
-            foreach (var scriptTemplate in builtInScriptTemplates) {
-                if (GUILayout.Button(scriptTemplate.name)) {
-                    selectedTemplate = scriptTemplate;
-                }
+            EditorGUILayout.EndScrollView();
+        }
+
+        private void DrawTemplateSelection() {
+            EditorGUILayout.LabelField("Selected template:");
+            string text;
+            if (selectedTemplate == null) {
+                text = "";
+            }
+            else {
+                if (selectedTemplate.isBuiltIn)
+                    text = "Built in/" + selectedTemplate.name;
+                else
+                    text = "Custom/" + selectedTemplate.name;
+            }
+
+            if (GUILayout.Button(text, GUILayout.Width(450f))) {
+                selectTemplateMenu.ShowAsContext();
             }
 
             if (selectedTemplate != null) {
-                var disabled = !hasWriteAccessToBuiltins && selectedTemplate.isBuiltIn;
-                if (disabled) {
-                    EditorGUILayout.LabelField("Cannot edit built in templates! You're probably not running Unity as administrator/root");
-                    EditorGUILayout.LabelField("Either relaunch as administrator/root, or edit the files directly in their folder");
-                }
-
-                EditorGUI.BeginDisabledGroup(disabled);
-                selectedTemplate.text = EditorGUILayout.TextArea(selectedTemplate.text);
-
-                EditorGUILayout.BeginHorizontal();
-
-                if (GUILayout.Button("Save to file", GUILayout.Width(120f))) {
-                    selectedTemplate.SaveToFile();
-                }
-
-                if (GUILayout.Button("Discard changes", GUILayout.Width(120f))) {
-                    selectedTemplate.DiscardChanges();
-                    GUI.FocusControl(null); //The text area doesn't update while not selected
-                    Repaint();
-                }
-
-                GUILayout.FlexibleSpace();
-
-                EditorGUILayout.LabelField("Indent style:", GUILayout.Width(100f));
-                if (GUILayout.Button("Spaces")) {
-                    selectedTemplate.text = selectedTemplate.text.Replace("\t", "    ");
-                }
-
-                if (GUILayout.Button("Tabs")) {
-                    selectedTemplate.text = selectedTemplate.text.Replace("    ", "\t");
-                }
-
-                EditorGUI.EndDisabledGroup();
-
-                EditorGUILayout.EndHorizontal();
+                EditorGUILayout.TextField("Path: " + selectedTemplate.path, GUI.skin.label); //textField so it's selectable for copy+pasting
             }
-            EditorGUILayout.EndScrollView();
+
+        }
+
+        private void DrawSelectedTemplate() {
+            var disabled = !hasWriteAccessToBuiltins && selectedTemplate.isBuiltIn;
+            if (disabled) {
+                var style = new GUIStyle(GUI.skin.label) {
+                    normal = {
+                        textColor = Color.red
+                    }
+                };
+                EditorGUILayout.LabelField("Cannot edit built in templates! You're probably not running Unity as administrator/root", style);
+                EditorGUILayout.LabelField("Either relaunch as administrator/root, or edit the files directly in their folder", style);
+            }
+
+            EditorGUI.BeginDisabledGroup(disabled);
+            selectedTemplate.text = EditorGUILayout.TextArea(selectedTemplate.text);
+
+            EditorGUILayout.BeginHorizontal();
+
+            if (GUILayout.Button("Save to file", GUILayout.Width(120f))) {
+                selectedTemplate.SaveToFile();
+            }
+
+            if (GUILayout.Button("Discard changes", GUILayout.Width(120f))) {
+                selectedTemplate.DiscardChanges();
+                GUI.FocusControl(null); //The text area doesn't update while not selected
+                Repaint();
+            }
+
+            GUILayout.FlexibleSpace();
+
+            EditorGUILayout.LabelField("Set indent style:", GUILayout.Width(100f));
+            if (GUILayout.Button("Spaces")) {
+                selectedTemplate.text = selectedTemplate.text.Replace("\t", "    ");
+            }
+
+            if (GUILayout.Button("Tabs")) {
+                selectedTemplate.text = selectedTemplate.text.Replace("    ", "\t");
+            }
+
+            EditorGUI.EndDisabledGroup();
+
+            EditorGUILayout.EndHorizontal();
         }
 
         private static List<ScriptTemplateFile> FindScriptTemplates(string templateFolder, bool isInternal) {
@@ -104,7 +148,7 @@ namespace UnityScriptTemplates {
     }
 
     public class ScriptTemplateFile {
-        private readonly string path;
+        public readonly string path;
         public readonly string name;
         public readonly bool isBuiltIn;
 
@@ -112,7 +156,7 @@ namespace UnityScriptTemplates {
         public string text;
 
         public ScriptTemplateFile(string path, bool isBuiltIn) {
-            this.path = path;
+            this.path = path.Replace("\\", "/");
             this.isBuiltIn = isBuiltIn;
             name = Path.GetFileNameWithoutExtension(path);
             fileText = File.ReadAllText(path);
--- README.md
+++ README.md
@@ -2,14 +2,21 @@
 Custom templates for creating new scripts in Unity.
 
 This project adds some entires under Create->Script Template for creating ScriptableObject and plain class files.
+There's also a window for editing custom and built-in templates.
 
 ![example](https://github.com/Baste-RainGames/Unity-Script-Templates/blob/master/README_IMG/ExampleImage.png)
 
 # Usage
 
-This works exactly like "Create->C# Script", except that the templates are placed in the Assets folder (in EditorDefaultResources) rather than in the Unity install folder. You can edit the templates however you like. To add a new template, see UnityScriptTemplates/CustomScriptTemplates.cs. It should be pretty easy to edit, as I've commented the code extensively. You should be able to add your own templates as needed, it only requites a couple of lines of code and a new template file.  You can also just use the code as an example to create your own tooling. Go wild.
+The custom templates work exactly like "Create->C# Script", except that the templates are placed in the Assets folder (in EditorDefaultResources) rather than in the Unity install folder. You can edit the templates however you like. To add a new template, see UnityScriptTemplates/CustomScriptTemplates.cs. It should be pretty easy to edit, as I've commented the code extensively. You should be able to add your own templates as needed, it only requites a couple of lines of code and a new template file.  You can also just use the code as an example to create your own tooling. Go wild.
 
-The templates works exactly like the ones found in Unity's install folder. There's some very light find-and-replace done to the text content in the file before it's used. You can check the behaviour by looking up ProjectWindowUtil.CreateScriptAssetFromTemplate from Unity's C# reference, but here's an overview:
+If you want to edit a template, open Window->Edit Script Templates:
+![example](https://github.com/Baste-RainGames/Unity-Script-Templates/blob/master/README_IMG/EditTemplateWindow.png)
+
+You can edit both the custom templates included in the project and the built-in ones. If you move your custom templates to somewhere else than the standard folder, you have to update EditScriptTemplates.cs. 
+Note that in order to edit built-in templates (like Create->C# Script), you have to launch Unity as Administrator if your Unity install is in a protected folder (like Program Files on Windows).
+
+Both the custom and built-in templates work by doing some very light find-and-replace to the text content in the template before it's copied. You can check the behaviour by looking up ProjectWindowUtil.CreateScriptAssetFromTemplate from Unity's C# reference, but here's an overview:
 #NOTRIM# is replaced with empty space. To quote Unity, " #NOTRIM# is a special marker that is used to mark the end of a line where we want to leave whitespace. prevent editors auto-stripping it by accident."
 #NAME# is replaced with the name you entered for the file
 #SCRIPTNAME# is replaced with the name you entered for the file, with spaces removed
@@ -20,5 +27,6 @@ The templates works exactly like the ones found in Unity's install folder. There
 # Notes
 Public domain lincense, so do whatever.
 I've been wanting this for a while, and after yet another person asked about it, I went and looked through the public C# source. Turns out it's not so hard to do this! Thanks to Unity for making the source available.
+I've tried to write this OS-independent, but I haven't tested it for OSX/Linux, so please tell me if it's broken on those platforms.
 
-If you've got problems with the code, just create an issue. If you have more general questions, you can reach me at baste@rain-games.com, or as "Baste" at the Unity forums. 
+If you've got bugs/issues/feature requests, create an issue! If you have more general questions, you can reach me at baste@rain-games.com, or as "Baste" at the Unity forums. 
